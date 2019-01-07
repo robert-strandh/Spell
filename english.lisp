3b9578d7f12208b672d562711171a1b5398594ed
Michal Herda <michal.herda@ericsson.com>
2019-01-07 10:54:36 +0100

    fix order in file

--- english.lisp
+++ english.lisp
@@ -1,10 +1,10 @@
 (in-package #:spell)
 
+(defparameter *english-dictionary*
+  #.(load-dictionary (asdf:system-relative-pathname :spell "english.txt")))
+
 (defun english-lookup (word)
   (let ((decapitalized (copy-seq word)))
     (setf (aref word 0) (char-downcase (aref word 0)))
     (or (lookup word *english-dictionary*)
         (lookup decapitalized *english-dictionary*))))
-
-(defparameter *english-dictionary*
-  #.(load-dictionary (asdf:system-relative-pathname :spell "english.txt")))

bbbbba9f13a22d5231ef6af02c8d2ad90ccdf16e
Michał "phoe" Herda <phoe@disroot.org>
2019-02-16 14:46:40 +0100

    Fix empty string in english-lookup

--- english.lisp
+++ english.lisp
@@ -1,10 +1,11 @@
 (in-package #:spell)
 
 (defun english-lookup (word)
-  (let ((decapitalized (copy-seq word)))
-    (setf (aref word 0) (char-downcase (aref word 0)))
-    (or (lookup word *english-dictionary*)
-        (lookup decapitalized *english-dictionary*))))
+  (when (and word (string/= word ""))
+    (let ((decapitalized (copy-seq word)))
+      (setf (aref word 0) (char-downcase (aref word 0)))
+      (or (lookup word *english-dictionary*)
+          (lookup decapitalized *english-dictionary*)))))
 
 (defparameter *english-dictionary*
   #.(load-dictionary (asdf:system-relative-pathname :spell "english.txt")))
